0ec531b8d6cc274fecaa47220363e40ded774526
shweper <32496204+shweper@users.noreply.github.com>
2024-09-01 10:02:53 +0300

    Add files via upload

---
+++ server.py
@@ -0,0 +1,19 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import socket
+
+
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        self.wfile.write(b"Hello from hostName: " + socket.gethostname().encode())
+
+def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler, port=80):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print(f"Starting httpd server on port {port}")
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    run()
