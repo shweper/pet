a174d2fcc71fcdc8e9510a34570d245d155bfc3b
shweper <shweper@ya.ru>
2024-09-06 07:55:03 +0300

    configure gitlab-runner

--- gitlabRunnerInstall.sh
+++ gitlabRunnerInstall.sh
@@ -1,12 +1,20 @@
+#!/usr/bin/bash
 # docker volume create gitlab-runner-config
+if [ -n "$1" ]; then
+  sudo docker run -d --name gitlab-runner --cpus="1" --memory="500m" --restart always -v /var/run/docker.sock:/var/run/docker.sock -v gitlab-runner-config:/etc/gitlab-runner gitlab/gitlab-runner:latest
 
-sudo docker run -d --name gitlab-runner --cpus="1" --memory="500m" --restart always -v /var/run/docker.sock:/var/run/docker.sock -v gitlab-runner-config:/etc/gitlab-runner gitlab/gitlab-runner:latest
+  IPHOST=$(hostname  -I | cut -f1 -d' ')
 
-sudo docker exec -it gitlab-runner gitlab-runner register -n \
-  --url "http://172.18.50.62/gitlab" \
-  --registration-token "GR1348941_KSDVrTj3Yr5Gd7HD_Uo" \
-  --executor docker \
-  --description "Runner" \
-  --docker-image "docker:24.0.5" \
-  --docker-volumes /var/run/docker.sock:/var/run/docker.sock
+  sudo docker exec -it gitlab-runner gitlab-runner register -n \
+    --url "http://${IPHOST}/gitlab" \
+    --registration-token "${1}" \
+    --executor docker \
+    --description "Runner" \
+    --docker-image "docker:24.0.5" \
+    --docker-volumes /var/run/docker.sock:/var/run/docker.sock
+else
+ echo "Not found parameters: registration-token"
+fi
+
+
 #sudo docker exec -it gitlab-runner gitlab-runner register
